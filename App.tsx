b69f7ae26104e7ca27bd974cfcc87d414aa14028
Raphael Flechtner <raphael@kilt.io>
2022-08-16 17:20:34 +0200

    first time working polyfills (-babel issue)

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import "./globals";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
---
+++ metro.config.js
@@ -0,0 +1,12 @@
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.sourceExts.push("cjs");
+
+defaultConfig.resolver.extraNodeModules = {
+  stream: require.resolve("stream-browserify"),
+  events: require.resolve("events"),
+};
+
+module.exports = defaultConfig;
