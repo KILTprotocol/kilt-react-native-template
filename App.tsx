615988547264f4c1fbb012608882fd35cbcb3869
dudleyneedham <dudleyneedham1993@gmail.com>
2022-08-17 14:14:41 +0200

    feat: adding linter and prettier

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import './globals';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import './globals'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+interface Styles {
+  container: {
+    flex: number
+    backgroundColor: string
+    alignItems: 'center'
+    justifyContent: 'center'
+  }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create<Styles>({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  )
+}
--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,12 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getDefaultConfig } = require('@expo/metro-config')
 
-const defaultConfig = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname)
 
-defaultConfig.resolver.sourceExts.push("cjs");
+defaultConfig.resolver.sourceExts.push('cjs')
 
 defaultConfig.resolver.extraNodeModules = {
-  stream: require.resolve("stream-browserify"),
-  events: require.resolve("events"),
-};
+  stream: require.resolve('stream-browserify'),
+  events: require.resolve('events'),
+}
 
-module.exports = defaultConfig;
+module.exports = defaultConfig
