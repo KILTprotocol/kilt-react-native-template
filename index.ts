07f8330d37602a4a08420bc4adc8633aae7baa27
dudleyneedham <dudleyneedham1993@gmail.com>
2022-08-17 20:10:50 +0200

    feat: adding index file to load

---
+++ index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import './globals'
+import { registerRootComponent } from 'expo'
+
+// @ts-ignore
+import { App } from './src/App'
+
+registerRootComponent(App)
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { init } from '@kiltprotocol/sdk-js'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
+export default function App() {
+  init()
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  )
+}
